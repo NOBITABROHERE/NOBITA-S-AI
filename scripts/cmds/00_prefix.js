60c71a736a761921390180bf0a117abc66aeee9f
Bhuvan <bhubanislam8@gmail.com>
2024-09-12 19:28:20 +0600

    Update 00_prefix.js

--- scripts/cmds/00_prefix.js
+++ scripts/cmds/00_prefix.js
@@ -1,13 +1,14 @@
 module.exports = {
  config: {
 	 name: "prefix",
+         alias: ["chiyoko","sonu"]
 	 version: "1.0",
-	 author: "Tokodori_Frtiz",//remodified by cliff
+	 author: "Tokodori_Frtiz",//remodified by Bhuban
 	 countDown: 5,
 	 role: 0,
 	 shortDescription: "no prefix",
 	 longDescription: "no prefix",
-	 category: "auto 🪐",
+	 category: "auto",
  },
 
  onStart: async function(){}, 
@@ -15,14 +16,14 @@ module.exports = {
  if (event.body && event.body.toLowerCase() === "prefix") {
  return message.reply({
  body: `
-Yo, my prefix is [ 𓆩 ✓ 𓆪 ]\n
+𝗔𝗦𝗦𝗔𝗟𝗔𝗠𝗨~𝗔𝗟𝗔𝗜𝗞𝗨𝗠\n 𝗠𝗬 𝗣𝗥𝗘𝗙𝗜𝗫 𝗜𝗦 [ # ]\n\n
 𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:
-➥ ✓help [number of page] -> see commands
-➥ ✓sim [message] -> talk to bot
-➥ ✓callad [message] -> report any problem encountered
-➥ ✓help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\nBot Developer: https://www.facebook.com/swordigo.swordslush`,
- attachment: await global.utils.getStreamFromURL("https://i.imgur.com/M4luPbE.gif")
+➥ #help [number of page] -> see commands
+➥ #sim [message] -> talk to bot
+➥ #callad [message] -> report any problem encountered
+➥ #help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\nBot Developer: https://www.facebook.com/swordigo.swordslush`,
+ attachment: await global.utils.getStreamFromURL("https://i.imgur.com/LJhJJ4E.jpeg")
  });
  }
  }
-}
+	 }
