9aa87232b4f37f60a1dd5d6cc369815eb5175189
Bhuvan <bhubanislam8@gmail.com>
2024-09-12 19:48:49 +0600

    Update prefix.js

--- scripts/cmds/00_prefix.js
+++ scripts/cmds/00_prefix.js
@@ -1,9 +1,9 @@
 module.exports = {
  config: {
 	 name: "prefix",
-	 alias:["chiyoko","bot"],
+	 alias:"chiyoko","bot",
 	 version: "1.0",
-	 author: "Tokodori_Frtiz",//remodified by Bhuban
+	 author: "Todoroki-Bhuban",//remodified by Bhuban
 	 countDown: 5,
 	 role: 0,
 	 shortDescription: "no prefix",
@@ -19,9 +19,9 @@ module.exports = {
 𝗔𝗦𝗦𝗔𝗟𝗔𝗠𝗨~𝗔𝗟𝗔𝗜𝗞𝗨𝗠\n 𝗠𝗬 𝗣𝗥𝗘𝗙𝗜𝗫 𝗜𝗦 [ # ]\n\n
 𝗦𝗢𝗠𝗘 𝗖𝗢𝗠𝗠𝗔𝗡𝗗𝗦 𝗧𝗛𝗔𝗧 𝗠𝗔𝗬 𝗛𝗘𝗟𝗣 𝗬𝗢𝗨:
 ➥ #help [number of page] -> see commands
-➥ #sim [message] -> talk to bot
+➥ #sonu [message] -> talk to bot
 ➥ #callad [message] -> report any problem encountered
-➥ #help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\nBot Developer: https://www.facebook.com/swordigo.swordslush`,
+➥ #help [command] -> information and usage of command\n\nHave fun using it enjoy!❤️\n\nBot Developer: https://www.facebook.com/PRIMES.LEGAL.FATHER`,
  attachment: await global.utils.getStreamFromURL("https://i.imgur.com/LJhJJ4E.jpeg")
  });
  }
